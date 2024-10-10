31994bd7eeeb498a401b8025e870fd002882ed5f
danielmwauramacharia <mwauradaniel06@gmail.com>
2024-10-10 19:54:01 +0300

    trial, fix eslint error

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,11 +1,13 @@
-import createEmployeesObject from './11-createEmployeesObject';
+// import createEmployeesObject from './11-createEmployeesObject';
 
 export default function createReportObject(employeesList) {
   const reports = {
     allEmployees: employeesList,
     getNumberOfDepartments: (allEmployees) => {
       let count = 0;
-      for (const key in allEmployees) count += 1;
+      for (const _ in allEmployees) {
+        count += 1;
+      };
       return count;
     },
   };
