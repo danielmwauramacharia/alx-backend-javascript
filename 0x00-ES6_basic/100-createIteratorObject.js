2ff3bb9bb3678fa8b927012cce0eb83736354b91
danielmwauramacharia <mwauradaniel06@gmail.com>
2024-10-10 22:55:24 +0300

    fix the eslint

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -2,12 +2,12 @@ export default function createIteratorObject(report) {
   const allEmployeesObj = report.allEmployees;
   const array = [];
   for (const value of Object.values(allEmployeesObj)) {
-    array.push(value)
+    array.push(value);
   }
   const arrayFinal = [];
   for (const item of array) {
     for (const items of item) {
-      arrayFinal.push(items)
+      arrayFinal.push(items);
     }
   }
   return arrayFinal;
