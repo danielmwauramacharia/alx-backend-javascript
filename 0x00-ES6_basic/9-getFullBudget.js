bd2fa716963d378d2bdb68b05ddc28b68628382c
danielmwauramacharia <mwauradaniel06@gmail.com>
2024-10-10 15:26:48 +0300

    fix arrow syntax

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -4,8 +4,8 @@ export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
   const fullBudget = {
     ...budget,
-    getIncomeInDollars: getIncomeInDollars = () => `$${income}`,
-    getIncomeInEuros: getIncomeInEuros = () => `${income} euros`,
+    getIncomeInDollars: () => `$${fullBudget.income}`,
+    getIncomeInEuros: () => `${fullBudget.income} euros`,
   };
   return fullBudget;
 };
