ce08d8248d9e5db7b85a47f65ce3e35182d20be1
danielmwauramacharia <mwauradaniel06@gmail.com>
2024-10-10 23:35:59 +0300

    try fix the un used var error

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,11 @@
-export default function taskBlock(trueOrFalse) {
+function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
 
   if (trueOrFalse) {
     const task = true;
     const task2 = false;
+    Object.prototype.hasOwnProperty.call(trueOrFalse, task, task2);
   }
   return [task, task2];
 }
