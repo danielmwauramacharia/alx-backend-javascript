c1c9535b241e7f62d6caaa8c06c8eddeece3b9b0
danielmwauramacharia <mwauradaniel06@gmail.com>
2024-10-10 16:41:57 +0300

    do the eslint changes

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
   for (const value of array) {
     const idx = array.indexOf(value);
-    const newValue = appendString + value;
-    array[idx] = newValue;
+    array[idx] = appendString + array[idx];
   }
   return array;
 }
