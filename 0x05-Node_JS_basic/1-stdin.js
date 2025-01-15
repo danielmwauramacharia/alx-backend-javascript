9b62df3130cb92c3bb69c575602edbdb016af296
danielmwauramacharia <mwauradaniel06@gmail.com>
2025-01-15 15:00:15 +0300

    adding the next code

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,13 +1,10 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
-
 process.stdin.on('readable', () => {
-  const uname = process.stdin.read();
-
-  if (uname) {
-    process.stdout.write(`Your name is: ${uname}`);
+  const name = process.stdin.read();
+  if (name !== null) {
+    process.stdout.write(`Your name is: ${name}`);
   }
 });
-
-process.stdin.on('close', () => {
+process.on('exit', () => {
   process.stdout.write('This important software is now closing\n');
 });
